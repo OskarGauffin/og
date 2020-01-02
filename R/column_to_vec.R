5cffdd39d85fb7cbbb8fc989b590ec3322d9b7a2
Oskar Gauffin <oskar.gauffin@who-umc.org>
2020-01-02 14:21:26 +0100

    First commit

---
+++ R/column_to_vec.R
@@ -0,0 +1,24 @@
+#' @title Column to vector
+#' Takes a column of strings and puts them into an R-vector.
+#'
+#' @description
+#' This function is used to convert a column of strings into an R-vector without inserting commas manually.
+#'
+#' @return
+#' The vector.
+#' # Example: Mark A, B, C  and press CTRL + C:
+#' # A
+#' # B
+#' # C
+# 
+# # Run:
+# rows_to_vec()
+# 
+# # Press CTRL + V:
+# c("A", "B", "C")
+#'
+#' @export
+column_to_vec <- function(){
+  names <- capture.output(dput(readClipboard()))
+  writeClipboard(names)
+}
---
+++ R/file_choose.R
@@ -0,0 +1,20 @@
+#' @title file_choose
+#' Inserts the selected string into the clipboard
+#'
+#' @description
+#' This function copies the selected string into the clipboard, to avoid using the mouse.
+#'
+#' @return
+#' Prints the selected string
+#' 
+#' file_choose()
+#'
+#' @export
+file.choose <- function(){
+  path <- gsub("\\", "/", base::file.choose(), fixed=T)
+  clipboard_path <- paste0('"', path,'"')
+  writeClipboard(clipboard_path)
+  cat("The following path has been saved to clipboard:", path)
+}
+
+
