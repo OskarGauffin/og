c5954b486bac7ffebce62ba919c9299a12f1f014
Oskar Gauffin <oskar.gauffin@who-umc.org>
2020-01-03 15:25:20 +0100

    The unit test doesn't work for linux

--- tests/testthat/test-test_column_to_vec.R
+++ tests/testthat/test-test_column_to_vec.R
@@ -1,5 +1,6 @@
-test_that("test columns to vector", {
+test_that("test column_to_vec", {
   
+skip_on_travis("column_to_vec only works on windows")
 writeClipboard(readLines(system.file("testdata", package="og")))
 column_to_vec()
 expect_equal(readClipboard(), 'c(\"A\", \"B\", \"C\")')
