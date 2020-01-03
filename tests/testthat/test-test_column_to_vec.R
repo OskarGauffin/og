41b5e17a80fac293f809ed65d7623a99df424815
Oskar Gauffin <oskar.gauffin@who-umc.org>
2020-01-03 15:39:18 +0100

    Debugging

--- tests/testthat/test-test_column_to_vec.R
+++ tests/testthat/test-test_column_to_vec.R
@@ -1,8 +1,12 @@
 test_that("test column_to_vec", {
   
-writeClipboard(readLines(system.file("testdata", package="og")))
-column_to_vec()
-skip_on_travis("column_to_vec only works on windows")
-expect_equal(readClipboard(), 'c(\"A\", \"B\", \"C\")')
-
+  test <- function(){
+    writeClipboard(readLines(system.file("testdata", package="og")))
+    column_to_vec()
+    readClipboard()
+  }
+  
+  skip_on_travis("column_to_vec only works on windows")
+  expect_equal(test(), 'c(\"A\", \"B\", \"C\")')
+  
 })
