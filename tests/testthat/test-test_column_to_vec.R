0b5d49a55818b4a5a7dbe41c7465f28a3a3078ba
Oskar Gauffin <oskar.gauffin@who-umc.org>
2020-01-03 15:31:24 +0100

    Debugging

--- tests/testthat/test-test_column_to_vec.R
+++ tests/testthat/test-test_column_to_vec.R
@@ -1,8 +1,8 @@
 test_that("test column_to_vec", {
   
-skip_on_travis("column_to_vec only works on windows")
 writeClipboard(readLines(system.file("testdata", package="og")))
 column_to_vec()
+skip_on_travis("column_to_vec only works on windows")
 expect_equal(readClipboard(), 'c(\"A\", \"B\", \"C\")')
 
 })
